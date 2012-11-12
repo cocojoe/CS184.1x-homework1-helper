ec7fa88bc76394f5e8472908f151f2520aaa07e7
Martin Walsh <martin.walsh@gmail.com>
2012-11-12 14:36:23 +0000

    Initial Commit

---
+++ homework1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  homework1
+//
+//  Created by Martin Walsh on 12/11/2012.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
